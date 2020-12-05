8b4df7b4249ba5e8b297b26b5aeeffaf04730c01
Michaël <14930982+geertsmichael@users.noreply.github.com>
2020-12-05 21:47:17 +0100

    Enable Telenet for BE and stop function (#21)
    * Added support for Telenet BE
    
    * improved stop
    
    * More Telenet support
    Supporting Channels by location
    
    * Added Authentication for Telenet
    
    Co-authored-by: Michaël Geerts <michael.geerts@ida-mediafoundry.be>

--- ziggonext/const.py
+++ ziggonext/const.py
@@ -27,22 +27,24 @@ MEDIA_KEY_CHANNEL_DOWN = "ChannelDown"
 
 MEDIA_KEY_RECORD = "MediaRecord"  # Not yet implemented
 MEDIA_KEY_PLAY_PAUSE = "MediaPlayPause"
-MEDIA_KEY_STOP = "MediaStop"  # Not yet implemented
+MEDIA_KEY_STOP = "MediaStop"
 MEDIA_KEY_REWIND = "MediaRewind"  # Not yet implemented
 MEDIA_KEY_FAST_FORWARD = "MediaFastForward"  # Not yet implemented
+
+BE_AUTH_URL = "https://login.prd.telenet.be/openid/login.do"
 
 COUNTRY_URLS_HTTP = {
     "nl": "https://web-api-prod-obo.horizon.tv/oesp/v4/NL/nld/web",
     "ch": "https://web-api-prod-obo.horizon.tv/oesp/v3/CH/eng/web",
-    "be-nl": "https://web-api-prod-obo.horizon.tv/oesp/v3/BE/nld/web",
-    "be-fr": "https://web-api-prod-obo.horizon.tv/oesp/v3/BE/fr/web",
+    "be-nl": "https://web-api-prod-obo.horizon.tv/oesp/v4/BE/nld/web",
+    "be-fr": "https://web-api-prod-obo.horizon.tv/oesp/v4/BE/fr/web",
     "at": "https://prod.oesp.magentatv.at/oesp/v3/AT/deu/web"
 }
 COUNTRY_URLS_PERSONALIZATION_FORMAT = {
     "nl": "https://prod.spark.ziggogo.tv/nld/web/personalization-service/v1/customer/{household_id}/devices",
     "ch": "https://prod.spark.upctv.ch/deu/web/personalization-service/v1/customer/{household_id}/devices",
-    "be-nl": "https://prod.spark.ziggogo.tv/nld/web/personalization-service/v1/customer/{household_id}/devices",
-    "be-fr": "https://prod.spark.ziggogo.tv/nld/web/personalization-service/v1/customer/{household_id}/devices",
+    "be-nl": "https://prod.spark.telenettv.be/nld/web/personalization-service/v1/customer/{household_id}/devices",
+    "be-fr": "https://prod.spark.telenettv.be/fr/web/personalization-service/v1/customer/{household_id}/devices",
     "at": "https://prod.spark.magentatv.at/deu/web/personalization-service/v1/customer/{household_id}/devices"
 } 
 
